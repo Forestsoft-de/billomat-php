e0b7a4f9b7b673496055daa8610a14eefe804f80
Sebastian Foerster <foerster@forestsoft.de>
2017-11-26 01:24:09 +0100

    Implement Delete Test

--- src/Client/IClient.php
+++ src/Client/IClient.php
@@ -13,10 +13,19 @@ interface IClient
 {
     public function getUri();
 
+    /**
+     * @param array $data
+     * @return IClient
+     */
     public function request($data = []);
 
     /**
      * @return mixed|\Zend\Stdlib\ParametersInterface
      */
     public function getQuery();
+
+    /**
+     * @return mixed
+     */
+    public function getResponse();
 }
